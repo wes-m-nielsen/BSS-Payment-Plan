06e14025e7a8a7b2919947a59f5477767007aa2c
wes-m-nielsen <wes.m.nielsen@gmail.com>
2024-12-22 19:20:35 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Payment Plan Calculator")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "For a customer intending to make regular repayments, enter the basic payment intention details and the app will calculate the full repayment schedule and clarify details."
 )
+
+# User Inputs
+
+st.divider()
+
+st.subheader("Details")
+
+total = st.number_input("Total amount to be repaid:")
+pay_freq = st.radio("Frequency of repayments:", ["Weekly", "Fortnightly", "Monthly", "Quarterly"])
+pay_amount = st.slider("Repayment amount:", 10, 1000, step = 5)
+start_date = st.date_input("Date of first re payment:", value="today")
+
+st.divider()
+
+st.button("Calculate")
+
+# Calculations
+
