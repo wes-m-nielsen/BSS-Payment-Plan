a8699a35311d53342f08600dcc3640f2b03e943e
wes-m-nielsen <wes.m.nielsen@gmail.com>
2025-01-16 04:35:14 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,42 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+from datetime import date
 
 st.title("Payment Plan Calculator")
-st.write(
-    "For a customer intending to make regular repayments, enter the basic payment intention details and the app will calculate the full repayment schedule and clarify details."
-)
+st.write("For a customer intending to make regular repayments, enter the basic payment intention details and the app will calculate the full repayment schedule and clarify details.")
 
-# User Inputs
+## USER INPUTS
 
 st.divider()
-
 st.subheader("Details")
 
-total = st.number_input("Total amount to be repaid:")
-pay_freq = st.radio("Frequency of repayments:", ["Weekly", "Fortnightly", "Monthly", "Quarterly"])
-pay_amount = st.slider("Repayment amount:", 10, 1000, step = 5)
-start_date = st.date_input("Date of first re payment:", value="today")
+purchase = int(st.number_input("Total amount to be repaid:"))
+pay_amount = int(st.slider("Repayment amount:", 10, 1000, step = 5))
+freq = st.radio("Frequency of repayments:", ["Weekly", "Fortnightly", "Four-weekly"])
+start_date = st.date_input("Date of first repayment:", value="today")
 
-st.divider()
+no_pay = purchase/pay_amount
 
-st.button("Calculate")
+if freq == "Weekly":
+    freq_index = 1
+elif freq == "Fortnightly":
+    freq_index = 2
+elif freq == "Four-weekly":
+    freq_index = 4
 
-# Calculations
+st.write("Starting on ", start_date, " there would be ", no_pay, " payments of ", pay_amount, " on a ", freq, "basis.")
+st.write(freq_index)
 
+## CALCULATIONS
+
+# Fee calculation
+if no_pay * freq_index >= 12:
+    fee_total = purchase * 0.07
+else: fee_total = 0
+
+fee_weekly = fee_total / 52
+end_date = start_date + timedelta(weeks = no_pay * freq_index + 1)
+
+st.write("A weekly fee of ", fee_weekly, " (or ", fee_total, " in total) will apply. The last payment will be on:", end_date)
